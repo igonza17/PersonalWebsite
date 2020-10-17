68ec06233c0170a2ae6e47d4ddff7cc398665e55
ivan <ivanxgv@gmail.com>
2020-10-17 16:37:42 -0400

    Updated to fix issues

--- final/settings.py
+++ final/settings.py
@@ -20,13 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '3a-losvxb_4bccjnn(%z^fl%&1r015&t09yd)v5cdxc=^b^0r!'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3a-losvxb_4bccjnn(%z^fl%&1r015&t09yd)v5cdxc=^b^0r!')
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+SECRET_KEY = '3a-losvxb_4bccjnn(%z^fl%&1r015&t09yd)v5cdxc=^b^0r!'
 
-ALLOWED_HOSTS = []
+#Mozilla tutorial
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3a-losvxb_4bccjnn(%z^fl%&1r015&t09yd)v5cdxc=^b^0r!')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+#Mozilla tutorial
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ['igonza17-website.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +132,21 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
 )
+
+# # Heroku: Update database configuration from $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/2.1/howto/static-files/
+#
+# # The absolute path to the directory where collectstatic will collect static files for deployment.
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#
+# # The URL to use when referring to static files (where they will be served from)
+# STATIC_URL = '/static/'
+#
+# # Simplified static file serving.
+# # https://warehouse.python.org/project/whitenoise/
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
